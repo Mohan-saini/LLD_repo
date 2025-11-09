da48bf2bbc4f4eab98920afd55afa956f53839f0
Mohan Lal Saini <lalsanmo@amazon.com>
2025-11-09 12:51:19 +0530

    Add code for simple logger system.

---
+++ logger/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+
+using namespace std;
+
+enum class LogLevel{
+    INFO,
+    DEBUG,
+    ERROR
+};
+
+string log_level_to_string(LogLevel level) {
+    switch(level) {
+        case LogLevel::INFO:
+            return "INFO";
+        case LogLevel::DEBUG:
+            return "DEBUG";
+        case LogLevel::ERROR:
+            return "ERROR";
+    }
+}
+
+class IAppender {
+    public:
+        virtual ~IAppender() = default;
+        virtual void log(string log_msg) = 0;
+};
+
+class ConsoleAppender : public IAppender {
+    public:
+        void log(string log_msg) override {
+            cout<<log_msg<<endl;
+        }
+};
+
+class Iformatter {
+    public:
+        virtual ~Iformatter() = default;
+        virtual string format(LogLevel level, string msg, const string &file, int line) = 0;
+};
+
+class LogFormatter : public Iformatter {
+    public:
+        string format(LogLevel level, string msg, const string &file, int line) override {
+            auto cur_time=chrono::system_clock::now();
+            string my_time = std::format("{:%Y-%m-%d %H:%M:%S}",cur_time);
+            return my_time+" "+to_string(line)+" "+log_level_to_string(level)+" "+msg;
+        }
+};
+
+class Logger {
+    public:
+        static Logger& getInstance() {
+            static Logger instance;
+            return instance;
+        }
+
+        void log(LogLevel level, string msg, const string &file, int line) {
+            msg = formatter->format(level, msg, file, line);
+            appender->log(msg);
+        }
+    private:
+        ~Logger() = default;
+        Logger(const Logger&) = delete;
+        Logger operator=(const Logger&) = delete;
+        unique_ptr<ConsoleAppender> appender;
+        unique_ptr<LogFormatter> formatter;
+        Logger() : appender(make_unique<ConsoleAppender>()),formatter(make_unique<LogFormatter>()) {}
+};
+
+int main()
+{
+    Logger::getInstance().log(LogLevel::INFO, "Hello World!!", __FILE__, __LINE__);
+    return 0;
+}
